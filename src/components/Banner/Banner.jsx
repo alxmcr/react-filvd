2aab6cd32ab7ea947a17f9d2de0ea616ac573a85
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-29 19:15:13 -0400

    * Working with CSS styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner() {
     return <main className="banner">
         <div className="banner__brand">
             <h1 className="banner__title">Filvd</h1>
-            <h3 className="banner__subtitle">Watch thousands of MOVIES</h3>
+            <h2 className="banner__subtitle"><i>Watch <u>thousands</u> of MOVIES</i></h2>
         </div>
         <div className="banner__actions">
             <a href="/login" className="banner__link banner__link--secondary">Login</a>
