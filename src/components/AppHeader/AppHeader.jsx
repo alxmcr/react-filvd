6e3763f2db97cdbcd0e1eb0702459372efee93a3
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-29 16:17:48 -0400

    * Refactor menus, extract

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -6,22 +6,19 @@ import './AppHeader.css'
 export default function AppHeader() {
     return <header>
         <nav className="header__nav" role="navigation" aria-label="main navigation">
-            <ul className="header__menu header__menu--brand">
-                <li className="header__items">
+            <ul className="header__submenu header__submenu--brand">
+                <li className="header__item">
                     <a href="/" className="header__link header__link--brand">Filvd</a>
                 </li>
-                <li className="header__items">
+                <li className="header__item">
+                </li>
+                <li className="header__item">
                     <i className="header__icon header__icon--menu">
                         <FontAwesomeIcon icon={faBars} />
                     </i>
                 </li>
             </ul>
-            <ul className="header__menu header__menu--mobile">
-                <li className="header__item">
-                    <i className="header__icon header__icon--close">
-                        <FontAwesomeIcon icon={faTimes} />
-                    </i>
-                </li>
+            <ul className="header__submenu header__submenu--desktop">
                 <li className="header__item">
                     <a href="/catalog" className="header__link">Catalog</a>
                 </li>
@@ -35,7 +32,12 @@ export default function AppHeader() {
                     <a href="/login" className="header__link header__link--login">Iniciar Sesion</a>
                 </li>
             </ul>
-            <ul className="header__menu header__menu--desktop">
+            <ul className="header__submenu header__submenu--mobile">
+                <li className="header__item">
+                    <i className="header__icon header__icon--close">
+                        <FontAwesomeIcon icon={faTimes} />
+                    </i>
+                </li>
                 <li className="header__item">
                     <a href="/catalog" className="header__link">Catalog</a>
                 </li>
