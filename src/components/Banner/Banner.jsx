b6be0d9ed6b9ce970081888e8da4d813134e94f3
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-29 19:11:24 -0400

    ! Banner and CSS styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,10 @@ import './Banner.css'
 
 export default function Banner() {
     return <main className="banner">
-        <h1 className="banner__title">Filvd</h1>
-        <h3 className="banner__subtitle">Watch thousands of movies</h3>
+        <div className="banner__brand">
+            <h1 className="banner__title">Filvd</h1>
+            <h3 className="banner__subtitle">Watch thousands of MOVIES</h3>
+        </div>
         <div className="banner__actions">
             <a href="/login" className="banner__link banner__link--secondary">Login</a>
             <a href="/register" className="banner__link banner__link--primary">Register</a>
