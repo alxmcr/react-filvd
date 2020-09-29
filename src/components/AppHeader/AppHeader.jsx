36b3252cdfe3467edcd67d25189c29c28b2081aa
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-29 15:50:36 -0400

    ! Refactor Menus: Brand, Desktop and Mobile

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -5,8 +5,8 @@ import './AppHeader.css'
 
 export default function AppHeader() {
     return <header>
-        <nav className="header__nav">
-            <ul className="header__list">
+        <nav className="header__nav" role="navigation" aria-label="main navigation">
+            <ul className="header__menu header__menu--brand">
                 <li className="header__items">
                     <a href="/" className="header__link header__link--brand">Filvd</a>
                 </li>
@@ -16,26 +16,39 @@ export default function AppHeader() {
                     </i>
                 </li>
             </ul>
+            <ul className="header__menu header__menu--mobile">
+                <li className="header__item">
+                    <i className="header__icon header__icon--close">
+                        <FontAwesomeIcon icon={faTimes} />
+                    </i>
+                </li>
+                <li className="header__item">
+                    <a href="/catalog" className="header__link">Catalog</a>
+                </li>
+                <li className="header__item">
+                    <a href="/movies/premiers" className="header__link">Premieres</a>
+                </li>
+                <li className="header__item">
+                    <a href="/subscription" className="header__link">Subscription</a>
+                </li>
+                <li className="header__item">
+                    <a href="/login" className="header__link header__link--login">Iniciar Sesion</a>
+                </li>
+            </ul>
+            <ul className="header__menu header__menu--desktop">
+                <li className="header__item">
+                    <a href="/catalog" className="header__link">Catalog</a>
+                </li>
+                <li className="header__item">
+                    <a href="/movies/premiers" className="header__link">Premieres</a>
+                </li>
+                <li className="header__item">
+                    <a href="/subscription" className="header__link">Subscription</a>
+                </li>
+                <li className="header__item">
+                    <a href="/login" className="header__link header__link--login">Iniciar Sesion</a>
+                </li>
+            </ul>
         </nav>
-
-        <ul className="header__menu">
-            <li className="header__item">
-                <i className="header__icon header__icon--close">
-                    <FontAwesomeIcon icon={faTimes} />
-                </i>
-            </li>
-            <li className="header__item">
-                <a href="/catalog" className="header__link">Catalog</a>
-            </li>
-            <li className="header__item">
-                <a href="/movies/premiers" className="header__link">Premieres</a>
-            </li>
-            <li className="header__item">
-                <a href="/subscription" className="header__link">Subscription</a>
-            </li>
-            <li className="header__item">
-                <a href="/login" className="header__link header__link--login">Iniciar Sesion</a>
-            </li>
-        </ul>
     </header>
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,8 +6,8 @@ export default function Banner() {
         <h1 className="banner__title">Filvd</h1>
         <h3 className="banner__subtitle">Watch thousands of movies</h3>
         <div className="banner__actions">
-            <a href="#" className="banner__link banner__link--secondary">Login</a>
-            <a href="#" className="banner__link banner__link--primary">Register</a>
+            <a href="/login" className="banner__link banner__link--secondary">Login</a>
+            <a href="/register" className="banner__link banner__link--primary">Register</a>
         </div>
     </main>
 }
