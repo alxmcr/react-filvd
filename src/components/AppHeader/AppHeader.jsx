d47f0f5266cdac1cb84e639a5ce9d3d8dc444e20
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-29 16:46:39 -0400

    * Fix HTML and CSS

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -6,11 +6,25 @@ import './AppHeader.css'
 export default function AppHeader() {
     return <header>
         <nav className="header__nav" role="navigation" aria-label="main navigation">
-            <ul className="header__submenu header__submenu--brand">
+            <ul className="header__menu  header__menu--desktop">
                 <li className="header__item">
                     <a href="/" className="header__link header__link--brand">Filvd</a>
                 </li>
                 <li className="header__item">
+                    <ul className="header__submenu header__submenu--desktop">
+                        <li className="header__item">
+                            <a href="/catalog" className="header__link">Catalog</a>
+                        </li>
+                        <li className="header__item">
+                            <a href="/movies/premiers" className="header__link">Premieres</a>
+                        </li>
+                        <li className="header__item">
+                            <a href="/subscription" className="header__link">Subscription</a>
+                        </li>
+                        <li className="header__item">
+                            <a href="/login" className="header__link header__link--login">Iniciar Sesion</a>
+                        </li>
+                    </ul>
                 </li>
                 <li className="header__item">
                     <i className="header__icon header__icon--menu">
@@ -18,21 +32,8 @@ export default function AppHeader() {
                     </i>
                 </li>
             </ul>
-            <ul className="header__submenu header__submenu--desktop">
-                <li className="header__item">
-                    <a href="/catalog" className="header__link">Catalog</a>
-                </li>
-                <li className="header__item">
-                    <a href="/movies/premiers" className="header__link">Premieres</a>
-                </li>
-                <li className="header__item">
-                    <a href="/subscription" className="header__link">Subscription</a>
-                </li>
-                <li className="header__item">
-                    <a href="/login" className="header__link header__link--login">Iniciar Sesion</a>
-                </li>
-            </ul>
-            <ul className="header__submenu header__submenu--mobile">
+
+            <ul className="header__menu header__menu--mobile">
                 <li className="header__item">
                     <i className="header__icon header__icon--close">
                         <FontAwesomeIcon icon={faTimes} />
