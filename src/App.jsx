07b44ecd90e21435dddf4e021c9aa59e23a6d008
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-29 13:03:26 -0400

    * Change file js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import AppHeader from './components/AppHeader/AppHeader';
 import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       <Banner />
     </div>
   );
